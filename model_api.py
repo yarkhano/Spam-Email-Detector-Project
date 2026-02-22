2517a6f389e4703e04dbb6f7c3a7a47edf6da37f
unknown <muhammadyarkhan31@gmail.com>
2026-02-22 11:13:32 -0800

    updated api description

--- model_api.py
+++ model_api.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+import pandas as pd
+import numpy as np
+import pickle
+from typing import List
+
 app = FastAPI(
     title= "This is a Spam email detector APi",
-    description="This is a Spam email detector API",
+    description="Created an api for spam detection project",
     version = "1.0.0"
 )
