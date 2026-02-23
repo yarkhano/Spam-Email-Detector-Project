7d146daa7687cc37222d024c431006575e7372c1
unknown <muhammadyarkhan31@gmail.com>
2026-02-23 09:50:51 -0800

    api is working smoothly however we hve to focus on model because it is I think overfitted

--- model_api.py
+++ model_api.py
@@ -180,4 +180,4 @@ async def predict_spam(input_data: input_message):
 
 # Running the app - Moved to the far left to fix IndentationError
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
